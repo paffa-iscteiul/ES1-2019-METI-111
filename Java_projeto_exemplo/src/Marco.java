04472ceedbcdac34947a2f58442a1dff7df44a52
mqsas-iscteiul <mqsas@iscte-iul.pt>
2019-10-04 14:44:45 +0100

    version1.0 marco

---
+++ Java_projeto_exemplo/src/Marco.java
@@ -0,0 +1,44 @@
+
+public class Marco {
+	public static int Vida;
+	public static int Stamina ;
+	public static int Strengt ;
+	public static int Atack ;
+	
+	public static void Marco (int vida, int stamina, int strengt) {
+		Vida=vida;
+		Stamina=stamina;
+		Strengt=strengt;
+		
+	}
+	public static int getVida() {
+		return Vida;
+	}
+	
+	public static int getStamina() {
+		return Stamina;
+	}
+	
+	public static int getStrengt() {
+		return Strengt;
+	}
+	
+	public static int getAtack() {
+		return Atack;
+	}
+	public static void Atack_calcule() {
+		Atack = (Strengt * Stamina)/Vida ;
+		
+	}
+	
+	public static void main(String[] args) {
+
+      Marco(100,175,300);
+      Atack_calcule();
+      getVida();
+      getStamina();
+      getStrengt();
+      getAtack();
+
+        }
+}
