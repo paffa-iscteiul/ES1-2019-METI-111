0ed80ecad02fa327e4788ec2f38916909467e5c5
paffa-iscteiul <paffa@iscte-iul.pt>
2019-10-10 20:47:07 +0100

    Mais um commit

--- Java_projeto_exemplo/src/Inês.java
+++ Java_projeto_exemplo/src/Inês.java
@@ -1,6 +1,6 @@
 
 public class Inês {
-	public static void main() {
+	public static void main(String[] args) {
 		System.out.println("Também já fiz!!!");
 	}
 }
--- Java_projeto_exemplo/src/classe_exemplo.java
+++ Java_projeto_exemplo/src/classe_exemplo.java
@@ -1,6 +1,6 @@
 
 public class classe_exemplo {
-	public static void main() {
+	public static void main(String[] args) {
 		System.out.println("Olá!!!");
 	}
 }
