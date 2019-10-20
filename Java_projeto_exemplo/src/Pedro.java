67f923ef7c514324315ca14916ff45cfc7e29a95
paffa-iscteiul <paffa@iscte-iul.pt>
2019-10-20 12:46:21 +0100

    Eliminação de exemplo de aulas

--- Java_projeto_exemplo/src/Pedro.java
+++
@@ -1,6 +0,0 @@
-
-public class Pedro {
-	public static void main(String[] args) {
-		System.out.println("Classe adicional!");
-	}
-}
--- Java_projeto_exemplo/src/Sofia.java
+++
@@ -1,9 +0,0 @@
-
-public class Sofia {
-
-	public static void main(String[] args) {
-		System.out.println("This is a test class");
-
-	}
-
-}
