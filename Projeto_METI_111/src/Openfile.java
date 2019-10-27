5ba35c5969afaf20055e38b3c1f9353ea28a731d
mqsas-iscteiul <mqsas@iscte-iul.pt>
2019-10-27 16:00:11 +0000

    testes de funcionamento

--- Projeto_METI_111/src/Openfile.java
+++ Projeto_METI_111/src/Openfile.java
@@ -14,6 +14,12 @@ public class Openfile {
 			//agora vamos buscar o ficheiro 
 			java.io.File file = escolherficheiro.getSelectedFile();
 			Ecxelfile = new File(file.getPath());
+			
+			
+			//leitura do ficheiro deve ser chamada aqui 
+			//sendo que a parte da ler o ficheiro ecxel nao me pertence para ver se relanet o ficheiro é carregado vou imprimir o path dele na consola 
+			System.out.println(Ecxelfile.getPath());
+			//com isto vi que funciona quando se fizer leitura do ficheiro é favor apagar a linha a cima
 		}
 			else {
 				System.out.println("nao selecionaram nenhum ficheiro");
