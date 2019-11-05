f37c66da68b2694a8019141f4d93f299b22fe65d
paffa-iscteiul <paffa@iscte-iul.pt>
2019-11-05 18:28:01 +0000

    Pequena atualização na criação da "Source Folder" src/test/java

---
+++ Projeto_METI_111/src/test/java/Projeto_METI_111/Projeto_METI_111/recordTest.java
@@ -0,0 +1,94 @@
+package Projeto_METI_111.Projeto_METI_111;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class recordTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testRecordStringStringStringStringStringStringStringStringStringStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRecord() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMethodID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPack() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetClasse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMethod() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLoc() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCyclo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAtfd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLaa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetIsLongMethod() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetIPlasma() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPmd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetIsFeatureEnvy() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRegisto() {
+		fail("Not yet implemented");
+	}
+
+}
